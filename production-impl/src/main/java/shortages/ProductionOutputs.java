38188f1dd333931d255b246029e76f4fc55d8536
Michal Michaluk <michal.michaluk@bottega.com.pl>
2022-02-09 15:58:49 +0100

    001-production-outputs

---
+++ production-impl/src/main/java/shortages/ProductionOutputs.java
@@ -0,0 +1,37 @@
+package shortages;
+
+import entities.ProductionEntity;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class ProductionOutputs {
+    private final String productRefNo;
+    private final Map<LocalDate, List<ProductionEntity>> outputs;
+
+    public ProductionOutputs(List<ProductionEntity> productions) {
+        HashMap<LocalDate, List<ProductionEntity>> outputs = new HashMap<>();
+        String productRefNo = null;
+        for (ProductionEntity production : productions) {
+            if (!outputs.containsKey(production.getStart().toLocalDate())) {
+                outputs.put(production.getStart().toLocalDate(), new ArrayList<>());
+            }
+            outputs.get(production.getStart().toLocalDate()).add(production);
+            productRefNo = production.getForm().getRefNo();
+        }
+        this.productRefNo = productRefNo;
+        this.outputs = Collections.unmodifiableMap(outputs);
+    }
+
+    public long outputsFor(LocalDate day) {
+        long level = 0;
+        for (ProductionEntity production : outputs.get(day)) {
+            level += production.getOutput();
+        }
+        return level;
+    }
+
+    public String getProductRefNo() {
+        return productRefNo;
+    }
+}
